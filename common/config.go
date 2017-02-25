b206a7dd2432bd99ee3a8cfa9a50862236bc36cd
Kazuki_Kamizuru <k.amago0721@gmail.com>
2017-02-26 00:38:42 +0900

    共通ライブラリ作成

---
+++ common/config.go
@@ -0,0 +1,72 @@
+package common
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"github.com/gotoeveryone/golang/logs"
+)
+
+type (
+	// Config 設定
+	Config struct {
+		Log   Log   `json:"log"`
+		Redis Redis `json:"redis"`
+		DB    DB    `json:"db"`
+		Mail  Mail  `json:"mail"`
+	}
+
+	// Log ログ設定
+	Log struct {
+		Path  string `json:"path"`
+		Level string `json:"level"`
+	}
+
+	// Redis Redis接続設定
+	Redis struct {
+		Host string `json:"host"`
+		Port int    `json:"port"`
+		Auth string `json:"auth"`
+	}
+
+	// DB データベース接続設定
+	DB struct {
+		Name     string `json:"name"`
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		User     string `json:"user"`
+		Password string `json:"password"`
+	}
+
+	// Mail メール接続設定
+	Mail struct {
+		SMTP     string   `json:"smtp"`
+		Port     int      `json:"port"`
+		User     string   `json:"user"`
+		Password string   `json:"password"`
+		From     string   `json:"from"`
+		To       []string `json:"to"`
+	}
+)
+
+var (
+	configDir = flag.String("conf", "", "config.json at directory")
+)
+
+// LoadConfig 設定をJSONファイルから読み込む
+func LoadConfig(config *Config) {
+	flag.Parse()
+	jsonValue, err := ioutil.ReadFile(*configDir + "config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal(jsonValue, &config); err != nil {
+		log.Fatal(err)
+	}
+	// ログ初期設定
+	if err := logs.Init(config.Log.Path, config.Log.Level); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ mail/mail.go
@@ -0,0 +1,36 @@
+package mail
+
+import (
+	"bytes"
+	"encoding/base64"
+	"net/smtp"
+	"strconv"
+	"strings"
+
+	"github.com/gotoeveryone/golang/common"
+)
+
+// SendMail メール送信
+func SendMail(config common.Config, subject string, body string) error {
+	// メール送信
+	auth := smtp.PlainAuth("", config.Mail.User, config.Mail.Password, config.Mail.SMTP)
+
+	var buffer bytes.Buffer
+	buffer.WriteString("From: " + config.Mail.From + "\n")
+	buffer.WriteString("To: " + strings.Join(config.Mail.To, ",") + "\n")
+	buffer.WriteString("Subject: ")
+	buffer.WriteString(" =?utf-8?B?")
+	buffer.WriteString(base64.StdEncoding.EncodeToString([]byte(subject)))
+	buffer.WriteString("?=\n")
+	buffer.WriteString("MIME-Version: 1.0\n")
+	buffer.WriteString("Content-Type: text/plain; charset=\"utf-8\"\n")
+	buffer.WriteString("Content-Transfer-Encoding: base64\n")
+	buffer.WriteString("\n")
+	buffer.WriteString(base64.StdEncoding.EncodeToString([]byte(body)))
+
+	str := config.Mail.SMTP + ":" + strconv.Itoa(config.Mail.Port)
+	if err := smtp.SendMail(str, auth, config.Mail.From, config.Mail.To, []byte(buffer.String())); err != nil {
+		return err
+	}
+	return nil
+}
