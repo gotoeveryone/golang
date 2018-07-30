5c6350522f1a34df308d691fa770e181c02f7f65
Kazuki_Kamizuru <k.amago0721@gmail.com>
2018-07-30 20:20:25 +0900

    add: logger struct and test

--- main.go
+++ main.go
@@ -9,53 +9,6 @@ import (
 	"path/filepath"
 )
 
-type (
-	// Config 設定
-	Config struct {
-		Log   Log   `json:"log"`
-		Cache Cache `json:"cache"`
-		DB    DB    `json:"db"`
-		Mail  Mail  `json:"mail"`
-	}
-
-	// Log ログ設定
-	Log struct {
-		Prefix string `json:"prefix"`
-		Path   string `json:"path"`
-		Level  string `json:"level"`
-	}
-
-	// Cache キャッシュサーバ接続設定
-	Cache struct {
-		Use  bool   `json:"use"`
-		Host string `json:"host"`
-		Port int    `json:"port"`
-		Auth string `json:"auth"`
-	}
-
-	// DB データベース接続設定
-	DB struct {
-		Name     string `json:"name"`
-		Host     string `json:"host"`
-		Port     int    `json:"port"`
-		User     string `json:"user"`
-		Password string `json:"password"`
-		Timezone string `json:"timezone"`
-	}
-
-	// Mail メール接続設定
-	Mail struct {
-		SMTP      string   `json:"smtp"`
-		Port      int      `json:"port"`
-		UseTLS    bool     `json:"useTLS"`
-		User      string   `json:"user"`
-		Password  string   `json:"password"`
-		From      string   `json:"from"`
-		FromAlias string   `json:"fromAlias"`
-		To        []string `json:"to"`
-	}
-)
-
 var (
 	configDir = flag.String("conf", "./", "config.json at directory")
 )
